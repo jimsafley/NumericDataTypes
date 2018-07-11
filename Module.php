e671db0455449e0e78e605ec059968213f0407f1
Jim Safley <jimsafley@gmail.com>
2018-07-11 11:35:59 -0400

    Move query preparation to separate method

--- Module.php
+++ Module.php
@@ -47,46 +47,7 @@ class Module extends AbstractModule
         $sharedEventManager->attach(
             'Omeka\Api\Adapter\ItemAdapter',
             'api.search.query',
-            function (Event $event) {
-                $query = $event->getParam('request')->getContent();
-                if (isset($query['ts'])) {
-                    $qb = $event->getParam('queryBuilder');
-                    $adapter = $event->getTarget();
-                    if (isset($query['ts']['lt']['ts']) && isset($query['ts']['lt']['pid'])) {
-                        $valuesAlias = $adapter->createAlias();
-                        $qb->leftJoin(
-                            $adapter->getEntityClass() . '.values',
-                            $valuesAlias,
-                            'WITH',
-                            $qb->expr()->andX(
-                                $qb->expr()->eq("$valuesAlias.type", "'timestamp'"),
-                                $qb->expr()->eq("$valuesAlias.property", (int) $query['ts']['lt']['pid'])
-                            )
-                        );
-                        $qb->andWhere($qb->expr()->lt(
-                            "$valuesAlias.value",
-                            $adapter->createNamedParameter($qb, $query['ts']['lt']['ts'])
-                        ));
-                    }
-                    if (isset($query['ts']['gt']['ts']) && isset($query['ts']['gt']['pid'])) {
-                        $valuesAlias = $adapter->createAlias();
-                        $qb->leftJoin(
-                            $adapter->getEntityClass() . '.values',
-                            $valuesAlias,
-                            'WITH',
-                            $qb->expr()->andX(
-                                $qb->expr()->eq("$valuesAlias.type", "'timestamp'"),
-                                $qb->expr()->eq("$valuesAlias.property", (int) $query['ts']['gt']['pid'])
-                            )
-                        );
-                        $qb->andWhere($qb->expr()->gt(
-                            "$valuesAlias.value",
-                            $adapter->createNamedParameter($qb, $query['ts']['gt']['ts'])
-                        ));
-                    }
-                }
-                //~ echo $qb->getDQL();exit;
-            }
+            [$this, 'prepareQuery']
         );
     }
 
@@ -95,5 +56,46 @@ class Module extends AbstractModule
         $view = $event->getTarget();
         //~ $view->headLink()->appendStylesheet($view->assetUrl('css/timestamp.css', 'Timestamp'));
         $view->headScript()->appendFile($view->assetUrl('js/timestamp.js', 'Timestamp'));
+    }
+
+    public function prepareQuery(Event $event)
+    {
+        $query = $event->getParam('request')->getContent();
+        if (isset($query['ts'])) {
+            $qb = $event->getParam('queryBuilder');
+            $adapter = $event->getTarget();
+            if (isset($query['ts']['lt']['ts']) && isset($query['ts']['lt']['pid'])) {
+                $valuesAlias = $adapter->createAlias();
+                $qb->leftJoin(
+                    $adapter->getEntityClass() . '.values',
+                    $valuesAlias,
+                    'WITH',
+                    $qb->expr()->andX(
+                        $qb->expr()->eq("$valuesAlias.type", "'timestamp'"),
+                        $qb->expr()->eq("$valuesAlias.property", (int) $query['ts']['lt']['pid'])
+                    )
+                );
+                $qb->andWhere($qb->expr()->lt(
+                    "$valuesAlias.value",
+                    $adapter->createNamedParameter($qb, $query['ts']['lt']['ts'])
+                ));
+            }
+            if (isset($query['ts']['gt']['ts']) && isset($query['ts']['gt']['pid'])) {
+                $valuesAlias = $adapter->createAlias();
+                $qb->leftJoin(
+                    $adapter->getEntityClass() . '.values',
+                    $valuesAlias,
+                    'WITH',
+                    $qb->expr()->andX(
+                        $qb->expr()->eq("$valuesAlias.type", "'timestamp'"),
+                        $qb->expr()->eq("$valuesAlias.property", (int) $query['ts']['gt']['pid'])
+                    )
+                );
+                $qb->andWhere($qb->expr()->gt(
+                    "$valuesAlias.value",
+                    $adapter->createNamedParameter($qb, $query['ts']['gt']['ts'])
+                ));
+            }
+        }
     }
 }
